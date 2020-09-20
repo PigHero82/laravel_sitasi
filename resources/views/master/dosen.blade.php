e61f671cbc5b7aba50bc73e1f4175b4507e97f08
Darma Wiryanata <45613994+ScrawnyGus@users.noreply.github.com>
2020-09-20 15:27:55 +0800

    Dosen detail modal update

--- resources/views/master/dosen.blade.php
+++ resources/views/master/dosen.blade.php
@@ -81,10 +81,10 @@
 
         <!-- Modal -->
         <div class="modal fade text-left" id="inlineForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel33" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg" role="document">
+            <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h4 class="modal-title" id="myModalLabel33">Inline Login Form </h4>
+                        <h4 class="modal-title" id="myModalLabel33">0898841078 | Detail Dosen</h4>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
