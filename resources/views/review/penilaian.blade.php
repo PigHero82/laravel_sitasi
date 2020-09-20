2fb702361a712684b6cd2291601d75568066faba
Darma Wiryanata <45613994+ScrawnyGus@users.noreply.github.com>
2020-09-20 16:25:09 +0800

    Penilaian view canvas

---
+++ resources/views/review/penilaian.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('css')
+    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('/app-assets/vendors/css/tables/datatable/datatables.min.css') }}"> --}}
+    {{-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.5/css/responsive.dataTables.min.css"> --}}
+@endsection
+
+@section('judul')
+    Penilaian
+@endsection
+
+@section('content')
+@endsection
+
+@section('js')
+@endsection
