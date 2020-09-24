40507d25e376cb8698166cbf379a1a40931c37ad
Darma Wiryanata <45613994+ScrawnyGus@users.noreply.github.com>
2020-09-24 12:14:13 +0800

    Login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,144 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+<!-- BEGIN: Head-->
 
-        <x-jet-validation-errors class="mb-4" />
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="PIXINVENT">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="apple-touch-icon" href="../../../app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../../../app-assets/images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/vendors.min.css') }}">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap-extended.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/colors.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/components.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/dark-layout.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/semi-dark-layout.css') }}">
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/core/menu/menu-types/vertical-menu.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/core/colors/palette-gradient.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/pages/authentication.css') }}">
+    <!-- END: Page CSS-->
+
+    <!-- BEGIN: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
+    <!-- END: Custom CSS-->
+
+</head>
+<!-- END: Head-->
+
+<!-- BEGIN: Body-->
+
+<body class="vertical-layout vertical-menu-modern 1-column  navbar-floating footer-static bg-full-screen-image  blank-page blank-page" data-open="click" data-menu="vertical-menu-modern" data-col="1-column">
+    <!-- BEGIN: Content-->
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper">
+            <div class="content-header row">
             </div>
-        @endif
+            <div class="content-body">
+                <section class="row flexbox-container">
+                    <div class="col-xl-8 col-11 d-flex justify-content-center">
+                        <div class="card bg-authentication rounded-0 mb-0">
+                            <div class="row m-0">
+                                <div class="col-lg-6 d-lg-block d-none text-center align-self-center px-1 py-0">
+                                    <img src="../../../app-assets/images/pages/login.png" alt="branding logo">
+                                </div>
+                                <div class="col-lg-6 col-12 p-0">
+                                    <div class="card rounded-0 mb-0 px-2">
+                                        <div class="card-header pb-1">
+                                            <div class="card-title">
+                                                <h4 class="mb-0">SITASI</h4>
+                                            </div>
+                                        </div>
+                                        <p class="px-2">Selamat datang, silahkan masuk dengan akun Anda</p>
+                                        <div class="card-content">
+                                            <div class="card-body pt-1">
+                                                @if (session('status'))
+                                                    <div class="mb-4 font-medium text-sm text-green-600">
+                                                        {{ session('status') }}
+                                                    </div>
+                                                @endif
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+                                                <form method="POST" action="{{ route('login') }}">
+                                                    @csrf
 
-            <div>
-                <x-jet-label value="NIDN" />
-                <x-jet-input class="block mt-1 w-full" type="number" name="nidn" :value="old('nidn')" required autofocus />
+                                                    <fieldset class="form-label-group form-group position-relative has-icon-left">
+                                                        <input type="number" class="form-control" id="user-name" placeholder="NIDN" name="nidn" :value="old('nidn')" required autofocus />
+                                                        <div class="form-control-position">
+                                                            <i class="feather icon-user"></i>
+                                                        </div>
+                                                        <label for="user-name">NIDN</label>
+                                                    </fieldset>
+
+                                                    <fieldset class="form-label-group position-relative has-icon-left">
+                                                        <input type="password" class="form-control" id="user-password" placeholder="Password" name="password" required autocomplete="current-password" />
+                                                        <div class="form-control-position">
+                                                            <i class="feather icon-lock"></i>
+                                                        </div>
+                                                        <label for="user-password">Password</label>
+                                                    </fieldset>
+                                                    <div class="form-group d-flex justify-content-between align-items-center">
+                                                        <div class="text-left">
+                                                            <fieldset class="checkbox">
+                                                                <div class="vs-checkbox-con vs-checkbox-primary">
+                                                                    <input type="checkbox" name="remember">
+                                                                    <span class="vs-checkbox">
+                                                                        <span class="vs-checkbox--check">
+                                                                            <i class="vs-icon feather icon-check"></i>
+                                                                        </span>
+                                                                    </span>
+                                                                    <span class="">Remember me</span>
+                                                                </div>
+                                                            </fieldset>
+                                                        </div>
+                                                        <button type="submit" class="btn btn-primary float-right btn-inline">Login</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
+        </div>
+    </div>
+    <!-- END: Content-->
 
-            <div class="mt-4">
-                <x-jet-label value="Password" />
-                <x-jet-input class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
 
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <input type="checkbox" class="form-checkbox" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+    <!-- BEGIN: Vendor JS-->
+    <script src="{{ asset('app-assets/vendors/js/vendors.min.js') }}"></script>
+    <!-- BEGIN Vendor JS-->
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+    <!-- BEGIN: Page Vendor JS-->
+    <!-- END: Page Vendor JS-->
 
-                <x-jet-button class="ml-4">
-                    {{ __('Login') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+    <!-- BEGIN: Theme JS-->
+    <script src="{{ asset('app-assets/js/core/app-menu.js') }}"></script>
+    <script src="{{ asset('app-assets/js/core/app.js') }}"></script>
+    <script src="{{ asset('app-assets/js/scripts/components.js') }}"></script>
+    <!-- END: Theme JS-->
+
+    <!-- BEGIN: Page JS-->
+    <!-- END: Page JS-->
+
+</body>
+<!-- END: Body-->
+
+</html>
--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -10,6 +10,12 @@
                     <li class="{{ (request()->is('admin/master/skema*')) ? 'active' : '' }}"><a href="{{ route('admin.master.skema') }}" title="Skema"><i></i><span class="menu-item">Skema</span></a></li>
                     <li class="{{ (request()->is('admin/master/rumpun-ilmu*')) ? 'active' : '' }}"><a href="{{ route('admin.master.rumpun-ilmu') }}" title="Rumpun Ilmu"><i></i><span class="menu-item">Rumpun Ilmu</span></a></li>
                     <li class="{{ (request()->is('admin/master/prodi*')) ? 'active' : '' }}"><a href="{{ route('admin.master.prodi') }}" title="Program Studi"><i></i><span class="menu-item">Program Studi</span></a></li>
+                    <li class="{{ (request()->is('admin/master/jabatan-akademik*')) ? 'active' : '' }}"><a href="#" title="Jabatan Akademik"><i></i><span class="menu-item">Jabatan Akademik</span></a></li>
+                    {{-- <li class="{{ (request()->is('admin/master/*')) ? 'active' : '' }}"><a href="#" title=""><i></i><span class="menu-item"></span></a></li>
+                    <li class="{{ (request()->is('admin/master/*')) ? 'active' : '' }}"><a href="#" title=""><i></i><span class="menu-item"></span></a></li>
+                    <li class="{{ (request()->is('admin/master/*')) ? 'active' : '' }}"><a href="#" title=""><i></i><span class="menu-item"></span></a></li>
+                    <li class="{{ (request()->is('admin/master/*')) ? 'active' : '' }}"><a href="#" title=""><i></i><span class="menu-item"></span></a></li>
+                    <li class="{{ (request()->is('admin/master/*')) ? 'active' : '' }}"><a href="#" title=""><i></i><span class="menu-item"></span></a></li> --}}
                 </ul>
             </li>
             <li class="nav-item"><a href="#" title="Review"><i class="feather icon-book"></i><span class="menu-title">Review</span></a>
@@ -20,6 +26,14 @@
             </li>
             <li class="{{ (request()->is('admin/skema')) ? 'active' : '' }} nav-item"><a href="{{ route('admin.skema') }}" title="Skema Usulan"><i class="feather icon-paperclip"></i><span class="menu-title">Skema Usulan</span></a></li>
             <li class="{{ (request()->is('admin/pimpinan*')) ? 'active' : '' }} nav-item"><a href="{{ route('admin.pimpinan') }}" title="Pimpinan & Koordinator"><i class="feather icon-user"></i><span class="menu-item">Pimpinan & Koordinator</span></a></li>
+            <li class="{{ (request()->is('admin/pengumuman*')) ? 'active' : '' }} nav-item"><a href="#" title="Pengumuman"><i class="feather icon-user"></i><span class="menu-item">Pengumuman</span></a></li>
+            {{-- <li class="{{ (request()->is('admin/*')) ? 'active' : '' }} nav-item"><a href="#" title=""><i class="feather icon-user"></i><span class="menu-item"></span></a></li>
+            <li class="{{ (request()->is('admin/*')) ? 'active' : '' }} nav-item"><a href="#" title=""><i class="feather icon-user"></i><span class="menu-item"></span></a></li>
+            <li class="{{ (request()->is('admin/*')) ? 'active' : '' }} nav-item"><a href="#" title=""><i class="feather icon-user"></i><span class="menu-item"></span></a></li>
+            <li class="{{ (request()->is('admin/*')) ? 'active' : '' }} nav-item"><a href="#" title=""><i class="feather icon-user"></i><span class="menu-item"></span></a></li>
+            <li class="{{ (request()->is('admin/*')) ? 'active' : '' }} nav-item"><a href="#" title=""><i class="feather icon-user"></i><span class="menu-item"></span></a></li>
+            <li class="{{ (request()->is('admin/*')) ? 'active' : '' }} nav-item"><a href="#" title=""><i class="feather icon-user"></i><span class="menu-item"></span></a></li>
+            <li class="{{ (request()->is('admin/*')) ? 'active' : '' }} nav-item"><a href="#" title=""><i class="feather icon-user"></i><span class="menu-item"></span></a></li> --}}
         
         @elseif (request()->is('dosen*'))
             <li class="{{ (request()->is('dosen')) ? 'active' : '' }} nav-item"><a href="{{ route('dosen.index') }}"><i class="feather icon-home"></i><span class="menu-title">Dashboard</span></a></li>
