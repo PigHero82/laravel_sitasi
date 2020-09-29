0b0f6f474fcb4afd74b37ddf012ed023f9154d39
Darma Wiryanata <45613994+ScrawnyGus@users.noreply.github.com>
2020-09-29 20:53:55 +0800

    Skema seed

--- database/seeders/SkemaSeeder.php
+++ database/seeders/SkemaSeeder.php
@@ -15,15 +15,63 @@ class SkemaSeeder extends Seeder
     public function run()
     {
         Skema::create([
-            'kode'  => '',
-            'nama'  => '',
-            'jenis' => 
+            'kode'  => 'PDP',
+            'nama'  => 'Penelitian Dosen Pemula',
+            'jenis' => 1
         ]);
         
         Skema::create([
-            'kode'  => '',
-            'nama'  => '',
-            'jenis' => 
+            'kode'  => 'PPDS Batch 1',
+            'nama'  => 'Penelitian Pengembangan Dosen STIKI',
+            'jenis' => 1
+        ]);
+        
+        Skema::create([
+            'kode'  => 'PKM STIKI Peduli (Tahunan) Batch 1',
+            'nama'  => 'Pengabdian Kepada Masyarakat',
+            'jenis' => 2
+        ]);
+        
+        Skema::create([
+            'kode'  => 'PKM STIKI Peduli (Insidental) Batch 1',
+            'nama'  => 'Pengabdian Kepada Masyarakat',
+            'jenis' => 2
+        ]);
+        
+        Skema::create([
+            'kode'  => 'PPDS Batch 2',
+            'nama'  => 'Penelitian Pengembangan Dosen STIKI Batch 2',
+            'jenis' => 1
+        ]);
+        
+        Skema::create([
+            'kode'  => 'PKM STIKI Peduli (Tahunan) Batch 2',
+            'nama'  => 'Pengabdian Kepada Masyarakat STIKI Peduli (Tahunan) Batch 2',
+            'jenis' => 2
+        ]);
+        
+        Skema::create([
+            'kode'  => 'PKM STIKI Peduli (Insidental) Batch 2',
+            'nama'  => 'Pengabdian Kepada Masyarakat STIKI Peduli (Insidental) Batch 2',
+            'jenis' => 2
+        ]);
+        
+        Skema::create([
+            'kode'  => 'PPDS',
+            'nama'  => 'Penelitian Pengembangan Dosen STIKI',
+            'jenis' => 1
+        ]);
+        
+        Skema::create([
+            'kode'  => 'PDM',
+            'nama'  => 'Penelitian Dosen Mahasiswa',
+            'jenis' => 1
+        ]);
+        
+        Skema::create([
+            'kode'  => 'SSE',
+            'nama'  => 'STIKI Social Engagement',
+            'jenis' => 2
         ]);
     }
 }
