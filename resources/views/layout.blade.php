722230a377c7525ba18f3776e8bd303c491c5b28
Leo Radhitya <leoibel@gmail.com>
2020-10-20 15:41:41 +0800

    change title dan icon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -125,7 +125,7 @@
                     @endif
                     ">
                         <div class="brand-logo"></div>
-                        <h2 class="brand-text mb-0">Vuexy</h2>
+                        <h2 class="brand-text mb-0">SITASI</h2>
                     </a></li>
                 <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block primary" data-ticon="icon-disc"></i></a></li>
             </ul>
