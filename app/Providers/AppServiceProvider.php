1e6b671deba01b956c346c4dc2860cdc825a5004
Darma Wiryanata <45613994+ScrawnyGus@users.noreply.github.com>
2020-10-26 06:41:32 +0800

    Bahasa indonesia configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,5 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        setlocale(LC_ALL, 'id');
+        \Carbon\Carbon::setLocale('id');
     }
 }
