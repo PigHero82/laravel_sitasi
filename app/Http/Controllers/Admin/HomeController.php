aee4e44b40b2df27d0d6e85ca41867aa4ad32e65
Darma Wiryanata <darmawiryanata@gmail.com>
2020-11-10 20:47:13 +0800

    Auto stash before merge of "main" and "origin/main"

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -15,9 +15,6 @@ class HomeController extends Controller
         $jum_pen = Usulan::where('jenis', 1)->count();
         $jum_peng = Usulan::where('jenis', 2)->count();
         $jum = [$jum_pen, $jum_peng];
- /*       $jum_pen = Usulan::where('jenis',1)->count();
-        $jum_peng = Usulan::where('jenis',2)->count();
-        $jum = [$jum_pen, $jum_peng];*/
         $tahun_penelitian = Usulan::selectRaw('skema_usulan.tahun_pelaksanaan, count(skema_usulan.tahun_pelaksanaan) as jumlah')->leftjoin('skema_usulan','usulan.skema_usulan_id','skema_usulan.id')->where('usulan.jenis',1)->groupBy('skema_usulan.tahun_pelaksanaan')->get();
         $tahun_pengabdian = Usulan::selectRaw('skema_usulan.tahun_pelaksanaan, count(skema_usulan.tahun_pelaksanaan) as jumlah')->leftjoin('skema_usulan','usulan.skema_usulan_id','skema_usulan.id')->where('usulan.jenis',2)->groupBy('skema_usulan.tahun_pelaksanaan')->get();
 
