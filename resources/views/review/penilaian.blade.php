df163b384a102ff98dbd52e758ef9ee7adcf042d
Leo Radhitya <leoibel@gmail.com>
2021-03-23 09:14:26 +0800

    ubah cdn jadi lokal

--- resources/views/review/penilaian.blade.php
+++ resources/views/review/penilaian.blade.php
@@ -475,9 +475,9 @@
 
 @section('js')
 <script src="{{ asset('app-assets/vendors/js/tables/datatable/datatables.min.js') }}"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+<script src="{{ asset('/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('/js/jszip.min.js') }}"></script>
+<script src="{{ asset('js/buttons.html5.min.js') }}"></script>
     <script src="{{ asset('app-assets/vendors/js/tables/datatable/datatables.bootstrap4.min.js') }}"></script>
 <script>
     $(document).ready( function () {
